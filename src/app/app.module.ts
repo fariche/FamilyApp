e00ec36fed5a04e9980bccf3e4b6f861985d3d93
Festus J. Ariche <fariche@corp.cgtglobal.com>
2018-11-02 14:53:22 -0700

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { MusicPage } from '../pages/music/music';
+import { NewsPage } from '../pages/news/news';
+import { VideoPage } from '../pages/video/video';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    MusicPage,
+    NewsPage,
+    VideoPage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    MusicPage,
+    NewsPage,
+    VideoPage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
